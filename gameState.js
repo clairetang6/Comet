8d6a80efaf28234ca6cc06e2689975d12212a676
Claire Tang <clairetang6@gmail.com>
2015-08-22 00:04:32 -0700

    added hero, sun, and moving spark particles

--- gameState.js
+++ gameState.js
@@ -2,23 +2,50 @@ var gameState = new Kiwi.State('gameState');
 
 gameState.preload = function() {
 	Kiwi.State.prototype.preload.call(this);
-	this.addSpriteSheet('hero_spritesheet', 'hero_spritesheet.png', 232, 200);
+	this.addSpriteSheet('hero_spritesheet', 'assets/hero/hero_spritesheet.png', 232, 200);
+	
+	this.addImage('sun1', 'assets/suns/sun_red2_242.png')
+	this.addImage('sparkParticle', 'assets/sparks/spark_particle_1.png')
 }
 
 gameState.create = function() {
 	Kiwi.State.prototype.create.call(this);
 	
+	this.random = this.game.rnd;
+	
+	
+	this.sun = new Sun(this, 200, 200, 1, 'sun1');
+	this.hero = new Hero(this, 100, 100);
+	
+	this.sparkParticles = new Kiwi.Group(this);
+	for (var i = 0; i < 100; i++){
+		this.sparkParticles.addChild(new SparkParticle(this));
+	}
+	
 	this.pauseKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.P);
-
+	this.debugKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.I);
+	
+	this.game.input.keyboard.onKeyDown.add(this.onKeyDownCallback, this);
+	
 	this.upKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.W);
 	this.leftKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.A);
 	this.rightKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.D);
 	this.downKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.S);	
 
+	this.particle = new SparkParticle(this);
+	this.addChild(this.sparkParticles);
+	this.addChild(this.particle);
+	this.addChild(this.sun);
+	this.addChild(this.hero);	
 }
 
 
 gameState.update = function() {
-
+	Kiwi.State.prototype.update.call(this);
 }
 
+gameState.onKeyDownCallback = function(keyCode){
+	if(keyCode == this.debugKey.keyCode){
+		this.hero.animation.play('death');
+	}
+}
--- suns.js
+++ suns.js
@@ -1,3 +1,27 @@
+var SparkParticle = function( state ){	
+	Kiwi.GameObjects.Sprite.call( this, state, state.textures['sparkParticle'], state.game.stage.width + 100, 0, false);
+	
+	this.state = state;
+	this.x = this.state.random.integerInRange(0, state.game.stage.width + 100);
+	this.y = this.state.random.integerInRange(0, state.game.stage.height);
+	this.scale = this.state.random.integerInRange(0, 10) / 10;
+	this.speed = this.scaleX * 2;
+	
+}
+Kiwi.extend( SparkParticle, Kiwi.GameObjects.Sprite );
+
+SparkParticle.prototype.update = function(){
+	Kiwi.GameObjects.Sprite.prototype.update.call(this);
+	
+	this.x -= this.speed;
+
+	if(this.x < -100){
+		this.x = this.state.game.stage.width + 200;
+		this.y = this.state.random.integerInRange(0, this.state.game.stage.height);	
+	}
+}
+
+
 var Sun = function( state, x, y, scale, textureKey ){
 	Kiwi.GameObjects.Sprite.call( this, state, state.textures[textureKey], x, y, false);
 	this.state = state;
@@ -20,11 +44,62 @@ Sun.prototype.update = function(){
 
 var Hero = function( state, x, y){
 	Kiwi.GameObjects.Sprite.call( this, state, state.textures['hero_spritesheet'], x, y, false);
-
+	
+	this.vx = 0;
+	this.vy = 0;
+	this.state = state;
+	
+	this.animation.add('chill', [0], 0.1);
+	this.animation.add('death', [0,1,2,3,4,5,6,7], 0.05);
 }
 Kiwi.extend( Hero, Kiwi.GameObjects.Sprite );
 
 Hero.prototype.update = function(){
 	Kiwi.GameObjects.Sprite.prototype.update.call(this);
+	
+	if(this.state.upKey.isDown){
+		if(this.vy > -50){
+			this.vy -= 1;
+		}
+	}
+	if(this.state.downKey.isDown){
+		if(this.vy < 50){
+			this.vy += 1;
+		}
+	}
+	if(this.state.rightKey.isDown){
+		if(this.vx < 50){
+			this.vx += 1;
+		}
+	}
+	if(this.state.leftKey.isDown){
+		if(this.vx > -50){
+			this.vx -= 1;
+		}
+	}
+
+	if(Math.abs(this.vy) > 0.0001){
+		this.vy = this.vy * 0.9
+	}else{
+		this.vy = 0;
+	}
+
+	if(Math.abs(this.vx) > 0.0001){
+		this.vx = this.vx * 0.9;
+	}else{
+		this.vx = 0;
+	}
+	
+	
+	this.y += this.vy; 
+	if(this.y < -100){
+		this.y = this.state.game.stage.height - this.height;
+	}else if(this.y > this.state.game.stage.height){
+		this.y = -100;
+	}
+	
+	if(this.x + this.vx > 0 && this.x + this.vx < this.state.game.stage.width){
+		this.x += this.vx;
+	}
 
 }
