349da9071c058f9e574a99fd0b71be1cfcfd7ec6
Claire Tang <clairetang6@gmail.com>
2015-08-24 20:14:15 -0700

    broke up hero and suns code into two files, added circle colliders to hero and sun

---
+++ hero.js
@@ -0,0 +1,193 @@
+var Hero = function( state, x, y, name){
+	Kiwi.Group.call( this, state);
+	this.x = x; 
+	this.y = y;
+	
+	this.comet = new Kiwi.GameObjects.Sprite( state, state.textures['hero_spritesheet'], 0, 0, false);
+	this.comet.animation.add('blue', [0], 0.1);
+	this.comet.animation.add('bluedeath', [0,1,2,3,4,5,6,7], 0.05);
+	
+	this.comet.animation.add('fire', [8], 0.1);
+	this.comet.animation.add('firedeath', [8,9,10,11,12,13,14,15], 0.05);
+	this.comet.animation.add('apple', [16], 0.1);
+	this.comet.animation.add('appledeath', [16,17,18,19,20,21,22,23], 0.05)
+	this.comet.name = name;
+	this.comet.animation.play(this.comet.name);
+	
+	this.tailGroup = new Kiwi.Group(state);
+
+	this.shadowScales = [0, 1];
+	this.shadowAlphas = [0, 1];
+	this.shadowOffsets = [0, 300];
+	this.numberOfShadows = 60;
+	for (var i = 0; i < this.numberOfShadows; i++){
+		var cometShadow = new CometShadow( state, this, i);
+		cometShadow.scale = (1 - i/this.numberOfShadows);
+		cometShadow.alpha = (1 - i/this.numberOfShadows); 
+		cometShadow.x = (0 - (6*i));
+		this.tailGroup.addChild(cometShadow);
+	}
+	
+	this.sparkGroup = new Kiwi.Group(state);
+	
+	this.numberOfSparks = 10;
+	for (var i = 0; i < this.numberOfSparks; i++){
+		var spark = new Spark(state, this, i);
+		this.sparkGroup.addChild(spark);
+	}
+	
+	this.addChild(this.tailGroup);	
+	this.addChild(this.sparkGroup);
+	this.addChild(this.comet);
+	
+	this.vx = 0;
+	this.vy = 0;
+	this.state = state;
+	
+	this.hitCircle = new Kiwi.Geom.Circle(this.comet.x + this.comet.width/2, this.comet.y + this.comet.height/2, 50);
+	this.isAlive = true;
+}
+Kiwi.extend( Hero, Kiwi.Group );
+
+var CometShadow = function( state , hero, index ){
+	Kiwi.GameObjects.Sprite.call(this, state, state.textures['hero_spritesheet'], 0, 3, false);	
+	
+	this.hero = hero; 
+	this.index = index;
+	
+	this.animation.add('bluetail', [7], 0.1, false);
+	this.animation.add('firetail', [15], 0.1, false);
+	this.animation.add('appletail', [23], 0.1, false);
+	this.animation.play(this.hero.comet.name + 'tail');
+}
+Kiwi.extend( CometShadow, Kiwi.GameObjects.Sprite);
+
+CometShadow.prototype.update = function(){
+	Kiwi.GameObjects.Sprite.prototype.update.call(this);
+	
+	this.index = this.index + 1; 
+	if(this.index >= this.hero.numberOfShadows -1){
+		this.index = 0;
+	}
+		
+	this.y = this.hero.vy * this.index * 0.04;
+	this.y -= this.hero.vy * Math.pow(this.index * 0.08, 2);
+
+	this.alpha = 1 - this.index/this.hero.numberOfShadows;
+	this.scaleX = 1 - this.index/this.hero.numberOfShadows;
+	this.scaleY = 1 - this.index/this.hero.numberOfShadows;
+
+	this.x = 0 - (6*this.index);
+	this.x -= this.hero.vx * Math.pow(this.index * 0.08, 2);
+	
+}
+
+var Spark = function(state, hero, index){
+	Kiwi.GameObjects.StaticImage.call(this, state, 'redSpark');
+	this.state = state;
+	this.hero = hero;
+	this.index = index;
+	this.startingX = this.hero.comet.width/2 - 20;
+	this.startingY = this.hero.comet.height/2 - 4; 
+	this.angle = Math.PI;
+}
+Kiwi.extend(Spark, Kiwi.GameObjects.StaticImage);
+
+Spark.prototype.update = function(){
+	Kiwi.GameObjects.StaticImage.prototype.update.call(this);
+	
+	this.index = this.index + 0.1; 
+	if(this.index >= this.hero.numberOfSparks -1){
+		this.index = 0;
+		this.x = this.startingX;
+		this.y = this.startingY;
+		this.setRandomAngle();
+	}
+	
+	this.x = this.index * 10 * Math.cos(this.angle) + this.startingX;
+	this.y = this.index * 10 * Math.sin(this.angle) + this.startingY;
+	this.x -= this.hero.vx * Math.pow(this.index * 0.12, 2);
+	this.y -= this.hero.vy * Math.pow(this.index * 0.12, 2);	
+
+	this.scaleX = 1 - this.index/this.hero.numberOfSparks;
+	this.scaleY = 1 - this.index/this.hero.numberOfSparks;
+		
+}
+
+Spark.prototype.setRandomAngle = function(){
+	var randomFrac = this.state.random.frac() / 3;
+	this.angle = (5 * Math.PI)/6 + (randomFrac * Math.PI);
+};
+
+
+Hero.prototype.die = function(){
+	this.comet.animation.play(this.comet.name + 'death');
+	this.isAlive = false;
+	//do something with his tail. 
+}
+
+Hero.prototype.checkCollisions = function(){
+	var sun = this.state.sun; 
+	if(this.hitCircle.distanceTo(sun.hitCircle) < (sun.hitCircle.radius + this.hitCircle.radius)){
+		if(this.isAlive){
+			this.die();
+		}
+	}else{
+		this.comet.animation.play('firechill');
+		this.isAlive = true;
+	}
+}
+
+Hero.prototype.update = function(){
+	Kiwi.Group.prototype.update.call(this);
+	
+	if(this.state.upKey.isDown){
+		if(this.vy > -50){
+			this.vy -= 1;
+		}
+	}
+	if(this.state.downKey.isDown){
+		if(this.vy < 50){
+			this.vy += 1;
+		}
+	}
+	if(this.state.rightKey.isDown){
+		if(this.vx < 50){
+			this.vx += 1;
+		}
+	}
+	if(this.state.leftKey.isDown){
+		if(this.vx > -50){
+			this.vx -= 1;
+		}
+	}
+
+	if(Math.abs(this.vy) > 0.0001){
+		this.vy = this.vy * 0.9
+	}else{
+		this.vy = 0;
+	}
+
+	if(Math.abs(this.vx) > 0.0001){
+		this.vx = this.vx * 0.9;
+	}else{
+		this.vx = 0;
+	}
+	
+	
+	this.y += this.vy; 
+	if(this.y < -100){
+		this.y = this.state.game.stage.height - this.height;
+	}else if(this.y > this.state.game.stage.height){
+		this.y = -100;
+	}
+	
+	if(this.x + this.vx > 0 && this.x + this.vx < this.state.game.stage.width){
+		this.x += this.vx;
+	}
+	
+	this.hitCircle.x = this.x + this.comet.width/2;
+	this.hitCircle.y = this.y + this.comet.height/2;
+	
+	this.checkCollisions();
+}
--- suns.js
+++ suns.js
@@ -71,6 +71,8 @@ var Planet = function( state, sun, textureKey ){
 	this.radius = 300;	
 	this.theta = 0; 
 	
+	this.hitCircle = new Kiwi.Geom.Circle(this.x + this.width/2, this.y + this.height/2, 115);
+	
 }
 Kiwi.extend( Planet, Kiwi.GameObjects.Sprite );
 
@@ -86,6 +88,8 @@ Planet.prototype.update = function(){
 	}	
 	
 	this.rotation += 0.02;
+	this.hitCircle.x = this.x + this.width/2;
+	this.hitCircle.y = this.y + this.height/2;
 }
 
 var Sun = function( state, x, y, scale, textureKey ){
@@ -100,7 +104,7 @@ var Sun = function( state, x, y, scale, textureKey ){
 
 	this.center = new Kiwi.Geom.Point(0,0);
 
-
+	this.hitCircle = new Kiwi.Geom.Circle(this.x + this.width/2, this.y + this.height/2, 210);
 }
 Kiwi.extend( Sun, Kiwi.GameObjects.Sprite );
 
@@ -110,179 +114,7 @@ Sun.prototype.update = function(){
 	this.center.x = this.x + this.width * 0.5;
 	this.center.y = this.y + this.height * 0.5;
 	this.rotation += 0.02;
+	this.hitCircle.x = this.x + this.width/2;
+	this.hitCircle.y = this.y + this.height/2;	
 }
 
-var Hero = function( state, x, y, name){
-	Kiwi.Group.call( this, state);
-	this.x = x; 
-	this.y = y;
-	
-	this.comet = new Kiwi.GameObjects.Sprite( state, state.textures['hero_spritesheet'], 0, 0, false);
-	this.comet.animation.add('blue', [0], 0.1);
-	this.comet.animation.add('bluedeath', [0,1,2,3,4,5,6,7], 0.05);
-	
-	this.comet.animation.add('fire', [8], 0.1);
-	this.comet.animation.add('firedeath', [8,9,10,11,12,13,14,15], 0.05);
-	this.comet.animation.add('apple', [16], 0.1);
-	this.comet.animation.add('appledeath', [16,17,18,19,20,21,22,23], 0.05)
-	this.comet.name = name;
-	this.comet.animation.play(this.comet.name);
-	
-	this.tailGroup = new Kiwi.Group(state);
-
-	this.shadowScales = [0, 1];
-	this.shadowAlphas = [0, 1];
-	this.shadowOffsets = [0, 300];
-	this.numberOfShadows = 60;
-	for (var i = 0; i < this.numberOfShadows; i++){
-		var cometShadow = new CometShadow( state, this, i);
-		cometShadow.scale = (1 - i/this.numberOfShadows);
-		cometShadow.alpha = (1 - i/this.numberOfShadows); 
-		cometShadow.x = (0 - (6*i));
-		console.log(cometShadow.x);
-		this.tailGroup.addChild(cometShadow);
-	}
-	
-	this.sparkGroup = new Kiwi.Group(state);
-	
-	this.numberOfSparks = 10;
-	for (var i = 0; i < this.numberOfSparks; i++){
-		var spark = new Spark(state, this, i);
-		this.sparkGroup.addChild(spark);
-	}
-	
-	this.addChild(this.tailGroup);	
-	this.addChild(this.sparkGroup);
-	this.addChild(this.comet);
-	
-	this.vx = 0;
-	this.vy = 0;
-	this.state = state;
-}
-Kiwi.extend( Hero, Kiwi.Group );
-
-var CometShadow = function( state , hero, index ){
-	Kiwi.GameObjects.Sprite.call(this, state, state.textures['hero_spritesheet'], 0, 3, false);	
-	
-	this.hero = hero; 
-	this.index = index;
-	
-	this.animation.add('bluetail', [7], 0.1, false);
-	this.animation.add('firetail', [15], 0.1, false);
-	this.animation.add('appletail', [23], 0.1, false);
-	this.animation.play(this.hero.comet.name + 'tail');
-}
-Kiwi.extend( CometShadow, Kiwi.GameObjects.Sprite);
-
-CometShadow.prototype.update = function(){
-	Kiwi.GameObjects.Sprite.prototype.update.call(this);
-	
-	this.index = this.index + 1; 
-	if(this.index >= this.hero.numberOfShadows -1){
-		this.index = 0;
-	}
-		
-	this.y = this.hero.vy * this.index * 0.04;
-	this.y -= this.hero.vy * Math.pow(this.index * 0.08, 2);
-
-	this.alpha = 1 - this.index/this.hero.numberOfShadows;
-	this.scaleX = 1 - this.index/this.hero.numberOfShadows;
-	this.scaleY = 1 - this.index/this.hero.numberOfShadows;
-
-	this.x = 0 - (6*this.index);
-	this.x -= this.hero.vx * Math.pow(this.index * 0.08, 2);
-	
-}
-
-var Spark = function(state, hero, index){
-	Kiwi.GameObjects.StaticImage.call(this, state, 'redSpark');
-	this.state = state;
-	this.hero = hero;
-	this.index = index;
-	this.startingX = this.hero.comet.width/2 - 20;
-	this.startingY = this.hero.comet.height/2 - 4; 
-	this.angle = Math.PI;
-}
-Kiwi.extend(Spark, Kiwi.GameObjects.StaticImage);
-
-Spark.prototype.update = function(){
-	Kiwi.GameObjects.StaticImage.prototype.update.call(this);
-	
-	this.index = this.index + 0.1; 
-	if(this.index >= this.hero.numberOfSparks -1){
-		this.index = 0;
-		this.x = this.startingX;
-		this.y = this.startingY;
-		this.setRandomAngle();
-	}
-	
-	this.x = this.index * 10 * Math.cos(this.angle) + this.startingX;
-	this.y = this.index * 10 * Math.sin(this.angle) + this.startingY;
-	this.x -= this.hero.vx * Math.pow(this.index * 0.12, 2);
-	this.y -= this.hero.vy * Math.pow(this.index * 0.12, 2);	
-
-	this.scaleX = 1 - this.index/this.hero.numberOfSparks;
-	this.scaleY = 1 - this.index/this.hero.numberOfSparks;
-		
-}
-
-Spark.prototype.setRandomAngle = function(){
-	var randomFrac = this.state.random.frac() / 3;
-	this.angle = (5 * Math.PI)/6 + (randomFrac * Math.PI);
-};
-
-
-Hero.prototype.die = function(){
-	this.comet.animation.play(this.comet.name + 'death');
-	//do something with his tail. 
-}
-
-Hero.prototype.update = function(){
-	Kiwi.Group.prototype.update.call(this);
-	
-	if(this.state.upKey.isDown){
-		if(this.vy > -50){
-			this.vy -= 1;
-		}
-	}
-	if(this.state.downKey.isDown){
-		if(this.vy < 50){
-			this.vy += 1;
-		}
-	}
-	if(this.state.rightKey.isDown){
-		if(this.vx < 50){
-			this.vx += 1;
-		}
-	}
-	if(this.state.leftKey.isDown){
-		if(this.vx > -50){
-			this.vx -= 1;
-		}
-	}
-
-	if(Math.abs(this.vy) > 0.0001){
-		this.vy = this.vy * 0.9
-	}else{
-		this.vy = 0;
-	}
-
-	if(Math.abs(this.vx) > 0.0001){
-		this.vx = this.vx * 0.9;
-	}else{
-		this.vx = 0;
-	}
-	
-	
-	this.y += this.vy; 
-	if(this.y < -100){
-		this.y = this.state.game.stage.height - this.height;
-	}else if(this.y > this.state.game.stage.height){
-		this.y = -100;
-	}
-	
-	if(this.x + this.vx > 0 && this.x + this.vx < this.state.game.stage.width){
-		this.x += this.vx;
-	}
-
-}
